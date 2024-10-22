59ce18cf99ce9949b7d2867b9e68c58e22e21128
Matti Leväsalmi <mattic.levasalmi@gmail.com>
2024-10-22 15:59:15 +0300

    Adding endpoint for registration

--- hotel-backend/index.js
+++ hotel-backend/index.js
@@ -1,4 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+
+var register = require('./src/register');
+app.post('/register', (req, res) => {
+    register(req, res);
+});
+
 
 app.listen(3000, () => console.log("Server is listening at port 3000"));
